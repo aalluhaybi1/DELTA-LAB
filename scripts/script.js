f21dc1a32da6172eb53d8c14f39e28a757496879
Ayman Alluhaybi <156353608+aalluhaybi1@users.noreply.github.com>
2024-02-20 11:45:38 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,21 @@ $(function() {
   console.log("Script is running");
 
   $("#btnGetFacts").click(function() {
-      console.log("Button clicked");
+    console.log("Button clicked");
 
-      let userName = $("#userName").val();
+    let userName = $("#userName").val();
 
-      let thisString = "Hello, " + (userName || "Undefined") + "!";
-      thisString += "<br><h3>'This is my fun Game'</h3>";
-      thisString += ` Your have (  ) letter `;
+    let thisString = "Hello, " + (userName || "Undefined") + "!";
+    thisString += "<br><h3>'This is my fun Game'</h3>";
 
-      $('#output').html(thisString);
+    // Counting the number of letters in userName
+    let letterCount = (userName || "").replace(/[^a-zA-Z]/g, "").length;
+    thisString += ` Your have (${letterCount}) letter `;
 
-      let modifiedString = manipulateString(thisString);
-      $('#output').append('<br><h3>Modified String:</h3>' + modifiedString);
+    $('#output').html(thisString);
+
+    let modifiedString = manipulateString(thisString);
+    $('#output').append('<br><h3>Modified String:</h3>' + modifiedString);
   });
 });
 
